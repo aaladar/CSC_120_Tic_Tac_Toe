33542b7b9df2f2fab9cb889cbc2a19a9875f0a65
Aryan Aladar <aryan@Aryans-MBP.attlocal.net>
2022-07-18 23:34:53 -0400

    stage 2 of the project

--- board.py
+++ board.py
@@ -1,14 +1,54 @@
-board = ['-','-','-',
-         '-','-','-',
-         '-','-','-']
-cont_game = True
-player_one = "X"
-winner = None
+board = [['_', '_', '_'],
+         ['_', '_', '_'],
+         ['_', '_', '_']]
+
+
+def playerOneMove():
+    try:
+        player_one_row = int(input("Player One... choose a row (0-2): "))
+        player_one_column = int(input("Player One... choose a column (0-2): "))
+        if board[player_one_row][player_one_column] != '_':
+            print("Space already occupied! Please try again.")
+            playerOneMove()
+        else:
+            board[player_one_row][player_one_column] = "X"
+            print_board()
+            if check_victory():
+                print("Player One Wins!")
+            elif check_tie():
+                print("No more available moves. It's a tie!")
+            else:
+                playerTwoMove()
+    except:
+        print("Invalid entry! Please try again.")
+        playerOneMove()
+
+
+def playerTwoMove():
+    try:
+        player_two_row = int(input("Player Two... choose a row (0-2): "))
+        player_two_column = int(input("Player Two... choose a column (0-2): "))
+        if board[player_two_row][player_two_column] != '_':
+            print("Space already occupied! Please try again.")
+            playerTwoMove()
+        else:
+            board[player_two_row][player_two_column] = "O"
+            print_board()
+            if check_victory():
+                print("Player Two Wins!")
+            elif check_tie():
+                print("No more available moves. It's a tie!")
+            else:
+                playerOneMove()
+    except:
+        print("Invalid entry! Please try again.")
+        playerTwoMove()
 
 def print_board():
-    print(board[0] + " | " + board[1] + " | " + board[2])
-    print(board[3] + " | " + board[4] + " | " + board[5])
-    print(board[6] + " | " + board[7] + " | " + board[8])
-    
+    pass
 
-  
+def check_victory():
+    pass
+
+def check_tie():
+    pass
